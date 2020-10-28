7b0816a8eea5f90aa30632ac3a4345233e0d805c
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-10-29 00:50:17 +1000

    Update get_size.py

--- get_size.py
+++ get_size.py
@@ -1,14 +1,17 @@
-from the_pile.datasets import WikipediaDataset, GutenbergDataset, PubMedCentralDataset
+from the_pile.datasets import WikipediaDataset, GutenbergDataset, PubMedCentralDataset, EnronEmailsDataset
 
 def main():
-    wiki = WikipediaDataset()
-    print("Wiki Size: ", wiki.size())
+    # wiki = WikipediaDataset()
+    # print("Wiki Size: ", wiki.size())
 
-    gutenberg = GutenbergDataset()
-    print("Gutenberg Size: ", gutenberg.size())
+    # gutenberg = GutenbergDataset()
+    # print("Gutenberg Size: ", gutenberg.size())
 
-    pubmed = PubMedCentralDataset()
-    pubmed._download()
+    # pubmed = PubMedCentralDataset()
+    # pubmed._download()
+
+    enron = EnronEmailsDataset()
+    enron._download()
 
 if __name__ == '__main__':
     main()
