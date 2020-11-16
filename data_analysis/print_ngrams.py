64e57fcb5b98c5a361948e6b6b261d5584182ed3
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-11-16 19:37:44 +1000

    Update print_ngrams.py

--- data_analysis/print_ngrams.py
+++ data_analysis/print_ngrams.py
@@ -10,6 +10,17 @@ logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser(description='n-gram statistics')
 parser.add_argument("-dir", "--working_directory", default="")
 
+def trim_ngram_dict(n_grams):
+    logger.info("Split finished, trimming dict.")
+    sorted_dict = {key: val for key, val in sorted(n_grams.items(), key = lambda ele: ele[1], reverse = True)}     
+    trimmed_dict = {}
+    for i, (n_gram, count) in enumerate(sorted_dict.items()):
+        if i == 1000:
+            break
+        trimmed_dict[n_gram] = count
+
+    return trimmed_dict
+
 def main(working_directory):
     ngrams_pickle_path = os.path.join(working_directory, "ngrams.pkl")
 
@@ -21,6 +32,7 @@ def main(working_directory):
 
     #     logger.info(f"{ngram} : {count}")
 
+    trimmed_ngrams = trim_ngram_dict(ngrams)
 
     csv_path = os.path.join(working_directory, "ngrams.csv")
     with open(csv_path, 'w', newline='') as csvfile:
@@ -28,10 +40,7 @@ def main(working_directory):
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        for i, (ngram, count) in enumerate(ngrams.items()):
-            if i == 1000:
-                break
-                
+        for ngram, count in trimmed_ngrams.items():
             writer.writerow({'ngram': ngram, 'count': count})
 
 if __name__ == '__main__':
