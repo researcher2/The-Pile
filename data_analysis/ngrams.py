5c0e030411ca395b7bff872a9aa2bb3239b456f2
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-11-17 04:33:34 +1000

    Update ngrams.py

--- data_analysis/ngrams.py
+++ data_analysis/ngrams.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import pickle
 import math
+import sys
 
 import tqdm
 import nltk
@@ -9,7 +10,7 @@ from nltk.util import ngrams
 from nltk.probability import FreqDist
 from tqdm_multiprocess import TqdmMultiProcessPool
 
-from the_pile.datasets import CommonCrawlDataset
+from the_pile.datasets import CommonCrawlDataset, OpenWebText2Dataset
 
 import logging
 from the_pile.logger import setup_logger_tqdm
@@ -51,15 +52,19 @@ def trim_ngram_dict(n_grams):
 
     return trimmed_dict
 
-def main(working_directory, process_count, n_value, approx_ram_gb):
+def dump_ngram_dict(working_directory, n_grams, dump_batch_number):
+    ngrams_pickle_file = os.path.join(working_directory, f"ngrams_{dump_batch_number}.pkl")
+    pickle.dump(n_grams, open(ngrams_pickle_file, "wb"))
+
+
+def main(working_directory, process_count, n_value, approx_ram_gb, dataset):
     nltk.download('punkt')
 
-    cc_dataset = CommonCrawlDataset()
     maximum_memory = approx_ram_gb * gigabyte
-    total_size = cc_dataset.size()
+    total_size = dataset.size()
     total_ngrams_size_worst = total_size * n_value
     split_count = math.ceil(total_ngrams_size_worst / maximum_memory)
-    documents_per_batch = cc_dataset.num_docs() / split_count
+    documents_per_batch = dataset.num_docs() / split_count
 
     logger.info(f"Allocated RAM: {maximum_memory:,} bytes")
     logger.info(f"Total CC Size: {total_size:,} bytes")
@@ -72,8 +77,9 @@ def main(working_directory, process_count, n_value, approx_ram_gb):
     pool = TqdmMultiProcessPool(process_count)
     count = 0
     n_grams = {}
-    with tqdm.tqdm(total=cc_dataset.num_docs(), dynamic_ncols=True, unit="docs") as progress:
-        for document, meta in cc_dataset.documents():
+    dump_batch_number = 0
+    with tqdm.tqdm(total=dataset.num_docs(), dynamic_ncols=True, unit="docs") as progress:
+        for document, meta in dataset.documents():
 
             batch.append(document)
 
@@ -84,19 +90,24 @@ def main(working_directory, process_count, n_value, approx_ram_gb):
                 count += batch_size
 
                 if count >= documents_per_batch:
-                    n_grams = trim_ngram_dict(n_grams)
+                    # n_grams = trim_ngram_dict(n_grams)
+                    dump_ngram_dict(n_grams, dump_batch_number)
+                    n_grams = {}
                     count = 0
+                    dump_batch_number += 1
 
 
         if len(batch) != 0:
             process_batch(pool, batch, n_value, n_grams)
+            dump_ngram_dict(n_grams, dump_batch_number)
             progress.update(len(batch))
 
-    pickle_file = os.path.join(working_directory, "ngrams.pkl")
-    pickle.dump(n_grams, open(pickle_file, "wb"))
+    # pickle_file = os.path.join(working_directory, "ngrams.pkl")
+    # pickle.dump(n_grams, open(pickle_file, "wb"))
 
 parser = argparse.ArgumentParser(description='n-gram statistics')
 parser.add_argument("-dir", "--working_directory", default="")
+parser.add_argument("-dir", "--dataset", default="cc")
 parser.add_argument("-procs", "--process_count", type=int, default=4)
 parser.add_argument("-n", "--n_value", type=int, default=13)
 parser.add_argument("-ram", "--approx_ram_gb", type=int, default=80)
@@ -106,4 +117,14 @@ if __name__ == '__main__':
     setup_logger_tqdm(logfile_path)
 
     args = parser.parse_args()
-    main(args.working_directory, args.process_count, args.n_value, args.approx_ram_gb)
+
+    if args.dataset == "cc":
+        logger.info("Dataset: Common Crawl")
+        dataset = CommonCrawlDataset()
+    elif args.dataset == "owt2":
+        dataset = OpenWebText2Dataset()
+    else:
+        logger.info("Invalid dataset")
+        sys.exit(-1)
+
+    main(args.working_directory, args.process_count, args.n_value, args.approx_ram_gb, dataset)
