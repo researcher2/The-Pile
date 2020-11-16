c3a6650f1d5065c0975a56e5a7b04834a3501d06
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-11-16 19:23:58 +1000

    Create print_ngrams.py

---
+++ data_analysis/print_ngrams.py
@@ -0,0 +1,27 @@
+import argparse
+import pickle
+import os
+
+import logging
+from the_pile.logger import setup_logger_tqdm
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser(description='n-gram statistics')
+parser.add_argument("-dir", "--working_directory", default="")
+
+def main(working_directory):
+    ngrams_pickle_path = os.join(working_directory, "ngrams.pkl")
+
+    ngrams = pickle.load(open(ngrams_pickle_path, "rb"))
+
+    for i, (ngram, count) in enumerate(ngrams.items()):
+        if i == 10:
+            break
+
+        logger.info(f"{ngram} : {count}")
+
+if __name__ == '__main__':
+    setup_logger_tqdm()
+
+    args = parser.parse_args()
+    main(args.working_directory)
