9cb3ba56514a4c27614097af8c0beae87f602c47
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-11-21 17:23:18 +1000

    Update n_grams_correct.py

--- data_analysis/n_grams_correct.py
+++ data_analysis/n_grams_correct.py
@@ -35,7 +35,7 @@ def process_batch(pool, batch, n_value, num_buckets):
 
     bucket_files = [None] * num_buckets
     for i in range(num_buckets):
-        bucket_file_path = os.path.join(working_directory, f"ngrams_{dataset_name}_i.bkt")
+        bucket_file_path = os.path.join(working_directory, f"ngrams_{dataset_name}_{i}.bkt")
         bucket_files[i] = open(bucket_file_path, "a")
 
     for result in results:
