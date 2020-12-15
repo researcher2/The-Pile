14a47037015d017814ff3154b15a8c0853a591e8
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-12-15 12:37:54 +1000

    Update process_statistics.py

--- data_analysis/process_statistics.py
+++ data_analysis/process_statistics.py
@@ -27,6 +27,8 @@ def main(working_directory, dataset_name, limit):
 
     # x = [21,22,23,4,5,6,77,8,9,10,31,32,33,34,35,36,37,18,49,50,100]
     x = list(range(0, cc_histogram_bucket_max, int(histogram_bucket_size)))
+    print(x)
+    print(frequencies)
     n, bins, patches = plt.hist(x, weights=frequencies, bins=histogram_bucket_count, facecolor='blue', alpha=0.5)
     # plt.show()
     plt.savefig(os.path.join(output_directory,'frequency_hist.png'))
